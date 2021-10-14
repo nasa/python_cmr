c581bd6f73c921e5de9ce848a31d6659f8a4be60
Frank Greguska <Francis.Greguska@jpl.nasa.gov>
2021-10-14 16:06:06 -0700

    Add long_description_content_type

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url="https://github.com/nasa/python_cmr",
     description="Python wrapper to the NASA Common Metadata Repository (CMR) API.",
     long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
     author="https://github.com/orgs/nasa/teams/python-cmr",
     packages=["cmr"],
     install_requires=[
